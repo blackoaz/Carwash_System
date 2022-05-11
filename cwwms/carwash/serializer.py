4a8565cf417e2f38edfa9bc1dc42879a8e2cc4c5
samwelkanda <samwelkanda@gmail.com>
2022-05-11 11:06:59 +0300

    Add carwash models

--- cwwms/carwash/serializer.py
+++ cwwms/carwash/serializer.py
@@ -1,9 +1,25 @@
 from rest_framework import serializers
 
-from .models import Vehicle
+from .models import Vehicle, Sale, SaleItem, Service
 
 class VehicleSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Vehicle
-        fields = ('unique_id', 'registration', 'body_type', 'created_at', 'update_at')
+        fields = ('uid', 'registration', 'vehicle_type', 'created_at', 'updated_at')
+        
+class SaleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vehicle
+        fields = '__all__'
+        
+class SaleItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SaleItem
+        fields = '__all__'
+        
+class ServiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Service
+        fields = '__all__'
         
